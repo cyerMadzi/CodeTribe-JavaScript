9604c6611d1d9021b0c1487500ca380f656de94b
CyberMadzi <Obvmulanga@gmail.com>
2025-04-11 03:00:51 +0200

    Data Types and Operators Finished

---
+++ datatypes_operators.js
@@ -0,0 +1,41 @@
+// Exercise 1 (Numbers)
+let num1 = 7;
+let num2 = 2.5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+let expeonetial = num1 ** num2; 
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
+console.log("Exponential:", expeonetial);
+
+// Exercise 2 (Boolean and Operators)
+let x = 8;
+let y = 12;
+
+console.log( x<=y, "'(Is x less than or equal to y)'");
+console.log( x>=y, "'(Is x greater than or equal to y)'");
+console.log( x==y, "'(Is x equal to y)'");
+console.log( x!=y, "'(Is x not equal to y)'");
+
+let a = true;
+let b = false;
+
+console.log( a && b, "'(a AND b)'");
+console.log( a || b, "'(a OR b)'");
+console.log( b =! a, "'(NOT b)'");
+
+let p = 10;
+
+console.log("added by 2: ", p+=2);
+console.log("subtracted by 2: ", p-=2);
+console.log("multiplied by 2: ", p*=2);
+console.log("divided by 2: ", p/=2);
+console.log("modulus when divided by 2: ", p%=2);
